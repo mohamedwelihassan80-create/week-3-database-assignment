d71a7cba9c2b32bcd7ed6b26afd774a2dc209070
Mohamedweli <mohamedwelihassan80@gmail.com>
2026-09-22 23:36:18 +0300

    Changed Undersores to double dashes and students table to student

--- answer.sql
+++ answer.sql
@@ -1,20 +1,20 @@
-_ _ Question 1 Create the students table
+-- Question 1 Create the students table
 
-CREATE TABLE students (
+CREATE TABLE student (
     id INT PRIMARY KEY ,
     fullName VARCHAR(100),
     age INT
 );
 
-_ _ Question 2 Inserting records in the students table
+-- Question 2 Inserting records in the students table
 
-INSERT INTO students (id, fullName, age) VALUES
+INSERT INTO student (id, fullName, age) VALUES
 (1, 'Musa Abdi Ahmed', 21),
 (2,  'Richard Otieno Ouma', 28),
 (3,  'Sarah Oyieko Ochoka', 30);
 
-_ _ Question 3  Updating age of a student
-UPDATE students
+-- Question 3  Updating age of a student
+UPDATE student
 SET age = 20
 WHERE id = 2;
 
